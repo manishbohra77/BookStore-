6290be41c989dd7298b2f70176453ded1d563414
Manish <mbohra789@gmail.com>
2024-07-20 01:42:46 +0530

    final changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,24 +2,26 @@ import Course from "./components/Course"
 import Signup from "./components/Signup"
 import Courses from "./courses/Courses"
 import Home from "./Home/Home"
-import { Routes,Route } from "react-router-dom"
+import { Routes,Route, Navigate } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
+import { useAuth } from "../context/AuthProvider"
 
 const App = () => {
+
+
+
+const [authUser,setAuthUser]=useAuth();
+console.log(authUser)
   return (
-   <>
-   {/* <Home/>
-   <Course/> */}
-
-
+   <> 
 <div className="dark:bg-slate-900 dark:text-white">
 <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/course' element={<Courses/>}/>
+        <Route path='/course' element={authUser ? <Courses/>:<Navigate to='/signup'/>}/>
         <Route path='/signup' element={<Signup/>}/>
       </Routes>
       <Toaster/>
-
+ 
 </div>
      
    </>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from '../context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-
-  <div className='dark:bg-slate-900 dark:text-white'>
+  <AuthProvider>
+  <div className="dark:bg-slate-900 dark:text-white">
   <App/>
   </div>
-
+  </AuthProvider>
   </BrowserRouter>  
  
 )
